8a690230c8fcc1cbd700d8e16216fa3c8d7275af
mtnapoleone <mnapoleone@wpi.edu>
2025-10-05 16:00:26 -0400

    fixing link, test

--- server.improved.js
+++ server.improved.js
@@ -42,7 +42,7 @@ passport.deserializeUser((obj, done) => done(null, obj));
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/github/callback"
+  callbackURL: process.env.GITHUB_CALLBACK_URL || "http://localhost:3000/auth/github/callback"
 }, (accessToken, refreshToken, profile, done) => {
   return done(null, profile);
 }));
